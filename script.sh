8899421a6dd3a870622c2eb134e4d8d9876cdc97
budougumi0617 <budougumi0617@binc.jp>
2021-02-05 03:09:11 +0900

    :recycle: formatted

--- script.sh
+++ script.sh
@@ -10,23 +10,17 @@ curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.s
 echo '::endgroup::'
 
 echo '::group:: Installing nrseg ... https://github.com/budougumi0617/nrseg'
-curl -L "$(curl -Ls https://api.github.com/repos/budougumi0617/nrseg/releases/latest | grep -o -E "https://.+?_Linux_x86_64.tar.gz")" -o nrseg.tar.gz
-tar -zxvf nrseg.tar.gz -C "${TEMP_PATH}"
-ls "${TEMP_PATH}"
-rm nrseg.tar.gz
+curl -L "$(curl -Ls https://api.github.com/repos/budougumi0617/nrseg/releases/latest | grep -o -E "https://.+?_Linux_x86_64.tar.gz")" -o nrseg.tar.gz && tar -zxvf nrseg.tar.gz -C "${TEMP_PATH}" && rm nrseg.tar.gz
 echo '::endgroup::'
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 echo '::group:: Running nrseg with reviewdog 🐶 ...'
-which reviewdog
-which nrseg
 # shellcheck disable=SC2086
-nrseg inspect ${INPUT_NRSEG_FLAGS}
 nrseg inspect ${INPUT_NRSEG_FLAGS} \
   | reviewdog -f=golint \
       -name="${INPUT_TOOL_NAME}" \
-      -reporter="${INPUT_REPORTER:-github-pr-review}" \
+      -reporter="${INPUT_REPORTER:-github-pr-check}" \
       -filter-mode="${INPUT_FILTER_MODE:-added}" \
       -fail-on-error="${INPUT_FAIL_ON_ERROR:-false}" \
       -level="${INPUT_LEVEL}" \
